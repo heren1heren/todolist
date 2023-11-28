050f4185b6922db1084f30537ec4a8922e00d9b8
Captain_Turtle <133596616+heren1heren@users.noreply.github.com>
2023-11-28 06:39:42 -0700

    planning out

--- src/script.js
+++ src/script.js
@@ -1,5 +1,22 @@
 import stylesheet from './style.css'
 import { injectingCardStructure,injectingListStructure,returnCardStructure,returnListStructure } from './InjectingHtmlStructure';
 
-injectingCardStructure()
+// injectingCardStructure()
 
+/* our plans:
+* storing lists inside an array
+* storing cards inside an array with each card as an object with an id and the description
+* let user push() new card
+* let user delete card with corresponding index of the button on the card
+* ... 
+*/
+
+let lists = [];
+let cards = [ 
+    {
+    id: 1,
+    description: 'Iam the test card, I am beautiful'
+}, {
+    id: 2,
+    description: 'I am strong, resilient, but still lazy'
+},];
